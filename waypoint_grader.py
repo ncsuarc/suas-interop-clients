2f6eb5646ebc0706b2e4fca3acba3d224cabe533
Justin Easter <jmeaste3@ncsu.edu>
2016-04-18 12:47:43 -0400

    Waypoint grader checks waypoint nav is accurate
    waypoint.csv is used to store waypoint locations.
    
    Change-Id: I536d609dcb0a5fa161c0dba9ef54ef62955a06fc

---
+++ waypoint_grader.py
@@ -0,0 +1,103 @@
+from ARC import unitconversion as uc
+
+import ARC
+import argparse
+import zmq
+import csv
+import pyproj
+import math
+
+piccolo_pub = 'ipc:///tmp/piccolo_pub'
+
+class Waypoint():
+    def __init__(self, lat, lon, alt):
+        self.lat = lat
+        self.lon = lon
+        self.alt = alt
+
+def generate_lat_lon(telem):
+    """
+    Looks up the needed data from the telemetry packet
+
+    Arguments:
+        telem: piccolo telemetry packet
+
+    Returns:
+        lat: gps latitude
+        lon: gps longitude
+        alt: gps altitude
+    """
+    lat = telem.sensors.gps.lat
+    lon = telem.sensors.gps.lon
+    alt = uc.m_to_ft(telem.sensors.gps.alt)
+    return lat, lon, alt
+
+def csv_to_waypoints(file):
+    """
+    Opens a comma seperated file and gets the waypoints
+
+    Arguments:
+        file: name of the file to open
+
+    Returns:
+        waypoints: list of waypoints
+    """
+    with open(file, 'r') as csvfile:
+        waypoints = []
+        csvreader = csv.reader(csvfile, delimiter=',')
+        for row in csvreader:
+            waypoints.append(Waypoint(float(row[0]), float(row[1]), float(row[2])))
+        return waypoints
+
+def waypoint_check(waypoints):
+    """
+    Checks that the plane flies the waypoints in order and within 50 ft
+
+    Arguments:
+        waypoints: list of waypoints
+
+    Returns:
+        None
+    """
+    try:
+        zmq_context = zmq.Context()
+        piccolo = zmq_context.socket(zmq.SUB)
+        piccolo.connect(piccolo_pub)
+        piccolo.setsockopt(zmq.SUBSCRIBE, "")
+        for waypoint in waypoints:
+            good = False
+            print(waypoint.lat, waypoint.lon)
+            way_location = ARC.presets.get_location(waypoint.lat, waypoint.lon)
+            way_coord = pyproj.transform(ARC.presets.wgs84,
+                                            way_location.projection,
+                                            waypoint.lon,
+                                            waypoint.lat)
+            while good == False:
+                packet = piccolo.recv_json()
+                telem = ARC.Telemetry(packet["telemetry"])
+                air_lat, air_lon, air_alt = generate_lat_lon(telem)
+                air_coord = pyproj.transform(ARC.presets.wgs84,
+                                                way_location.projection,
+                                                air_lon,
+                                                air_lat)
+                delta = (way_coord[0] - air_coord[0],
+                        way_coord[1] - air_coord[1])
+                dist = math.sqrt(delta[0]**2 + delta[1]**2)
+                dist = uc.m_to_ft(dist)
+                alt_delta = abs(waypoint.alt - air_alt)
+                if dist < 50 and alt_delta < 50:
+                    print("~~~~~~~~~WAYPOINT MET~~~~~~~~~~")
+                    good = True
+                print(dist, alt_delta)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        piccolo.close()
+        zmq_context.term()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Judges waypoint accuracy')
+    parser.add_argument('-f', '--file', type=str, required=True, help="Waypoint file in csv format")
+    args = parser.parse_args()
+    waypoints = csv_to_waypoints(args.file)
+    waypoint_check(waypoints)
