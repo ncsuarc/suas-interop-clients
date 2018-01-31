bac5d0c608d603c12508b6b35d346fb7d61b0203
Colin Moore <colinmoore716@gmail.com>
2018-01-31 12:32:11 -0500

    Removed print statements, Added raise_for_status
    Removed print statements from internal functions.
    Added raise_for_status calls on server responses to indicate errors.
    
    Change-Id: Id65f1d9ced7728289ddcd98e79ed1c07ac7b2dcb

--- interop.py
+++ interop.py
@@ -14,40 +14,48 @@ class Interop(object):
     def __init__(self):
         self.session = requests.session()
         settings = QSettings("ARC", "PCC Interop Plugin")
-        self.ip = str(settings.value('host'))
+        self.host = str(settings.value('host'))
         self.port = str(settings.value('port'))
         username = str(settings.value('username'))
         password = str(settings.value('password'))
 
         payload = {'username': username, 'password': password}
-        response = self.session.post("http://%s:%s/api/login"%(self.ip, self.port), data = payload, timeout = 5)
-        print(response.text)
+        self.session.post("http://%s:%s/api/login"%(self.host, self.port),
+                          data=payload, timeout=5).raise_for_status()
 
     def get_missions(self):
-        missions = self.session.get('http://%s:%s/api/missions'%(self.ip, self.port))
+        missions = self.session.get('http://%s:%s/api/missions'%(self.host, self.port))
+        missions.raise_for_status()
         return missions.json()
 
     def get_obstacles(self):
-        obstacles = self.session.get('http://%s:%s/api/obstacles'%(self.ip, self.port))
+        obstacles = self.session.get('http://%s:%s/api/obstacles'%(self.host, self.port))
+        obstacles.raise_for_status()
         return obstacles.json()
 
     def get_targets(self):
-        targets = self.session.get('http://%s:%s/api/targets'%(self.ip, self.port))
+        targets = self.session.get('http://%s:%s/api/targets'%(self.host, self.port))
+        targets.raise_for_status()
         return targets.json()
 
     def delete_target(self, i):
-        self.session.delete('http://%s:%s/api/targets/%d/image'%(self.ip, self.port, i))
-        self.session.delete('http://%s:%s/api/targets/%d'%(self.ip, self.port, i))
+        self.session.delete('http://%s:%s/api/targets/%d/image'
+                            %(self.host, self.port, i)).raise_for_status()
+        self.session.delete('http://%s:%s/api/targets/%d'
+                            %(self.host, self.port, i)).raise_for_status()
 
     def post_target(self, target_json, image):
-        interop_target_data = self.session.post('http://%s:%s/api/targets'%(self.ip, self.port), json = target_json)
-        target_id = interop_target_data.json().get('id')
-        img_ret = self.session.post('http://%s:%s/api/targets/%d/image'%(self.ip, self.port, target_id), data = image)
+        target_data = self.session.post('http://%s:%s/api/targets'
+                                        %(self.host, self.port),
+                                        json=target_json)
+        target_data.raise_for_status()
+        target_id = target_data.json().get('id')
+        self.session.post('http://%s:%s/api/targets/%d/image'
+                          %(self.host, self.port, target_id),
+                          data=image).raise_for_status()
         return target_id
 
     def send_coord(self, lat, lon, alt, heading):
         coord = {'latitude': lat, 'longitude': lon, 'altitude_msl': alt, 'uas_heading': heading}
-        plane_location = self.session.post("http://%s:%s/api/telemetry"%(self.ip, self.port), data = coord)
-        print(plane_location.text)
-
-
+        plane_location = self.session.post("http://%s:%s/api/telemetry"%(self.host, self.port), data = coord)
+        plane_location.raise_for_status()
