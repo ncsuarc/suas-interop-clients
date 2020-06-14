9e349936eded29b66398fcfa09b2dced42406e03
Eli W. Hunter <42009212+elihunter173@users.noreply.github.com>
2020-06-13 23:32:08 -0400

    Upgrade geo module to new interop API (#4)

--- interop_clients/geo/boundary.py
+++ interop_clients/geo/boundary.py
@@ -1,16 +1,15 @@
 from osgeo import ogr  # type: ignore
 
-from interop_clients import InteropClient
+from interop_clients import api
 
 
-def check_point(client: InteropClient, lat: float, lon: float) -> bool:
-    """
-    Check if point is within the boundary of an active mission.
+def in_search_grid(mission: api.Mission, lat: float, lon: float) -> bool:
+    """Check if point is within the boundary of an active mission.
 
     Args:
-        client: The InteropClient object.
-        lat: The float value of your Latitude coordinate.
-        lon: The float value of your Longitude coordinate.
+        mission: Mission to check the search grid of.
+        lat: Latitude of coordinate to check.
+        lon: Longitude of coordinate to check.
 
     Returns:
         True if point is within boundary, otherwise false
@@ -18,26 +17,12 @@ def check_point(client: InteropClient, lat: float, lon: float) -> bool:
     target_point = ogr.Geometry(ogr.wkbPoint)
     target_point.AddPoint(lat, lon)
 
-    missions = client.get_missions()
-    for mission in missions:
-        if bool(mission.get("active")):
-            active_mission = mission
-            break
-    interop_grid_points = active_mission.get("search_grid_points")
-    grid_points = [None] * len(interop_grid_points)
-
-    for point in interop_grid_points:
-        grid_points[point.get("order") - 1] = (
-            point.get("latitude"),
-            point.get("longitude"),
-        )
-
+    grid_points = mission["search_grid_points"]
     ring = ogr.Geometry(ogr.wkbLinearRing)
-
     for point in grid_points:
-        ring.AddPoint(point[0], point[1])
+        ring.AddPoint(point["latitude"], point["longitude"])
+    ring.AddPoint(grid_points[0]["latitude"], grid_points[0]["longitude"])
 
-    ring.AddPoint(grid_points[0][0], grid_points[0][1])
     polygon = ogr.Geometry(ogr.wkbPolygon)
     polygon.AddGeometry(ring)
 
--- interop_clients/geo/cli.py
+++ interop_clients/geo/cli.py
@@ -10,17 +10,19 @@ def main() -> None:
     """
 
 
-@main.command("check")
+@main.command("in-search")
+@click.argument("mission_id", type=int)
 @click.argument("lat", type=float)
 @click.argument("lon", type=float)
 @click.pass_context
-def check_point(ctx: click.Context, lat: float, lon: float) -> None:
-    """
-    Check if LAT and LON are within active mission boundaries.
+def in_search(
+    ctx: click.Context, mission_id: int, lat: float, lon: float
+) -> None:
+    """Check if LAT and LON are within active mission boundaries.
     """
     client = ctx.obj
     click.echo("Is the point within the boundaries? ", nl=False)
-    if boundary.check_point(client, lat, lon):
+    if boundary.in_search_grid(client.get_mission(mission_id), lat, lon):
         click.secho("Yes", fg="blue")
     else:
         click.secho("No", fg="red")
