4c55c13611610e53e4bcb638b9b34cd53f3f4725
Eli W. Hunter <elihunter173@gmail.com>
2020-06-14 13:06:43 -0400

    tests: Make smoke tests support Python 3.6

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -6,6 +6,6 @@ import interop_clients
 def test_version():
     # Ensure that interop was at least properly installed
     proc = subprocess.run(
-        ["interop", "--version"], check=True, capture_output=True
+        ["interop", "--version"], check=True, stdout=subprocess.PIPE
     )
     assert interop_clients.__version__.encode() in proc.stdout
